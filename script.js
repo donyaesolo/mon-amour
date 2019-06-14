496ce0d3f5dd68b7aeeae94e1243905c151dcd32
donyaesolo <35720283+donyaesolo@users.noreply.github.com>
2019-06-14 15:48:41 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+  function timer(){
+    console.log("PEEEEEEENIS");
+    t = document.getElementById("timer");
+    var countDownDate = new Date("Jun 5, 2019");
+    var now = new Date();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate + now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+     document.getElementById("timer").innerHTML =  "Love You";
+   }
